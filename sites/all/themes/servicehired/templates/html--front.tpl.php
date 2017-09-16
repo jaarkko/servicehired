45c0abe23069734d761c18fef7fe0c5a24d69254
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-16 21:49:50 +0300

    CSS

--- sites/all/themes/servicehired/templates/html--front.tpl.php
+++ sites/all/themes/servicehired/templates/html--front.tpl.php
@@ -324,6 +324,9 @@
         <path d="M7 0C3.13 0 0 3.13 0 7c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5C5.62 9.5 4.5 8.38 4.5 7S5.62 4.5 7 4.5 9.5 5.62 9.5 7 8.38 9.5 7 9.5z"></path>
       </svg>
     </div>
+      <div class="search-buttons">
+          <div class="btn">Search now</div>
+      </div>
   </div>
   <div class="menu">
     <div class="leftmenu">
