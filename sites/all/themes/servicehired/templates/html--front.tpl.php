06a7f7f46ef11e840427ff7960387265b3691e46
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-20 18:25:57 +0300

    Servicehired

--- sites/all/themes/servicehired/templates/html--front.tpl.php
+++ sites/all/themes/servicehired/templates/html--front.tpl.php
@@ -20,7 +20,8 @@
     <link rel="apple-touch-icon-precomposed" href="/sites/all/themes/servicehired/icons2/Icon-72@2x.png" sizes="144x144" />
     <link rel="apple-touch-icon-precomposed" href="/sites/all/themes/servicehired/icons2/Icon-76@2x.png" sizes="152x152" />
     <link rel="apple-touch-icon-precomposed" href="/sites/all/themes/servicehired/icons2/Icon-60@3x.png" sizes="180x180" />
-  <style>
+    <script src="https://js.stripe.com/v3/"></script>
+    <style>
       footer li a {
           text-decoration: none;
           font-size: 0.8em !important;
@@ -1477,6 +1478,59 @@
 
 
   helper.search();
+
+  var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+  var elements = stripe.elements();
+
+  var card = elements.create('card', {
+    hidePostalCode: true,
+    style: {
+      base: {
+        iconColor: '#F99A52',
+        color: '#32315E',
+        lineHeight: '48px',
+        fontWeight: 400,
+        fontFamily: '"Helvetica Neue", "Helvetica", sans-serif',
+        fontSize: '15px',
+
+        '::placeholder': {
+          color: '#CFD7DF',
+        }
+      },
+    }
+  });
+  card.mount('#card-element');
+
+  function setOutcome(result) {
+    var successElement = document.querySelector('.success');
+    var errorElement = document.querySelector('.error');
+    successElement.classList.remove('visible');
+    errorElement.classList.remove('visible');
+
+    if (result.token) {
+      // Use the token to create a charge or a customer
+      // https://stripe.com/docs/charges
+      successElement.querySelector('.token').textContent = result.token.id;
+      successElement.classList.add('visible');
+    } else if (result.error) {
+      errorElement.textContent = result.error.message;
+      errorElement.classList.add('visible');
+    }
+  }
+
+  card.on('change', function(event) {
+    setOutcome(event);
+  });
+
+  document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var form = document.querySelector('form');
+    var extraDetails = {
+      name: form.querySelector('input[name=cardholder-name]').value,
+      address_zip: form.querySelector('input[name=address-zip]').value
+    };
+    stripe.createToken(card, extraDetails).then(setOutcome);
+  });
 </script>
 </body>
 </html>
