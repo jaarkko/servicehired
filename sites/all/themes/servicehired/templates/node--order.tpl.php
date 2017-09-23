bc68e32ccdc55f2172c10047a6a285cb53799a22
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-24 01:41:52 +0300

    IF 4

--- sites/all/themes/servicehired/templates/node--order.tpl.php
+++ sites/all/themes/servicehired/templates/node--order.tpl.php
@@ -155,8 +155,10 @@ foreach ($results['node'] as $result){
                     </div>
                 </div>
                 <div class="actions">
-                    <div class="confirm btn confirm-<?php print $node->field_status['und'][0]['value']; ?>">Confirm availability</div>
-                    <a href="/developers/cancel/<?php print $node->nid; ?>"><div class="canceled btn">Cancel service</div></a>
+                    <?php if($node->field_status['und'][0]['value'] !== '4'): ?>
+                        <div class="confirm btn confirm-<?php print $node->field_status['und'][0]['value']; ?>">Confirm</div>
+                        <a href="/developers/cancel/<?php print $node->nid; ?>"><div class="canceled btn">Cancel service</div></a>
+                    <?php endif; ?>
                 </div>
                 <div class="clearfix"></div>
             </div>
