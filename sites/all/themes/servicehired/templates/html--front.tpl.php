04c0040b475eaa50d7d97e85a14808ff2dd09a51
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-24 19:20:28 +0300

    CSS

--- sites/all/themes/servicehired/templates/html--front.tpl.php
+++ sites/all/themes/servicehired/templates/html--front.tpl.php
@@ -668,10 +668,17 @@
     <div class="leftmenu">
       <a href="https://www.inventshift.com/blog">Blog</a>
     </div>
+      <?php if (user_is_logged_in()): ?>
     <div class="rightmenu">
         <a class="listyourbusiness" href="/list-your-business">List your business</a>
       <a href="/user">Sign In</a>
     </div>
+      <?php endif; ?>
+    <?php if (!user_is_logged_in()): ?>
+        <div class="rightmenu">
+            <a href="/user/logout">Log Out</a>
+        </div>
+    <?php endif; ?>
   </div>
   <div class="bottom-menu">
     <ul>
