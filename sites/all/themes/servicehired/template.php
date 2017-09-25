412a9de6bb008db1601d0af779d1820dfbc54628
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-25 20:12:03 +0300

    Revert "Try reverts"
    This reverts commit 0357c96b62014452f5e44d1863983068766ea78d.

--- sites/all/themes/servicehired/template.php
+++ sites/all/themes/servicehired/template.php
@@ -7,5 +7,13 @@
 
 function servicehired_preprocess_html(&$variables) {
   // Create a dedicated attributes array for the BODY element.
+  global $user;
 
+  ddl($user);
+  if($user->roles[2] == 'authenticated user') {
+    $variables['servicehired_logged'] = 'logged';
+  } else {
+    $variables['servicehired_logged'] = 'anon';
+  }
+  ddl($variables['servicehired_logged']);
 }
