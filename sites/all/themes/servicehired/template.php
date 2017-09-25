e2c303b048869324ad41cd1c39771ce5b9803034
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-25 20:00:32 +0300

    Logged?

--- sites/all/themes/servicehired/template.php
+++ sites/all/themes/servicehired/template.php
@@ -7,12 +7,13 @@
 
 function servicehired_preprocess_html(&$variables) {
   // Create a dedicated attributes array for the BODY element.
-  if (!isset($variables['servicehired_logged'])) {
-    global $user;
-    if($user->roles[2] == 'authenticated user') {
-      $variables['servicehired_logged'] = 'logged';
-    } else {
-      $variables['servicehired_logged'] = 'anon';
-    }
+  global $user;
+
+  ddl($user);
+  if($user->roles[2] == 'authenticated user') {
+    $variables['servicehired_logged'] = 'logged';
+  } else {
+    $variables['servicehired_logged'] = 'anon';
   }
+  ddl($variables['servicehired_logged']);
 }
