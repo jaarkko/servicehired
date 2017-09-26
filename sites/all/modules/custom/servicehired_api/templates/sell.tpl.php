ca974e13a9eed91de0bf3b32db3c534cfd3f0eca
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-26 23:31:41 +0300

    Dev

--- sites/all/modules/custom/servicehired_api/templates/sell.tpl.php
+++ sites/all/modules/custom/servicehired_api/templates/sell.tpl.php
@@ -4,29 +4,30 @@
 
 <div class="order-flow">
     <div class="left-col"><h3>How does Servicehired work?</h3>
-        <p>Write a story here.</p>
+        <p>1. Lets start with the basics</p>
+        <p>We want your business details. This allows our thousands
+        of visitors to see and find you. Make sure to add a few
+        high quality images.</p>
     </div>
     <div class="right-col">
-  <h3>Start selling now</h3>
-    <div class="btn btn-change">Add your business</div>
-    </div>
+  <h3>Add your business</h3>
+      <?php
+
+      global $user;
+      module_load_include('inc', 'node', 'node.pages');
+      $node = (object) array(
+        'uid' => $user->uid,
+        'name' => (isset($user->name) ? $user->name : ''),
+        'type' => 'supplier',
+        'language' => LANGUAGE_NONE,
+      );
+      $form = drupal_get_form('supplier_node_form', $node);
+
+      print drupal_render($form)
+
+      ?>
 </div>
 
-<?php
-
-global $user;
-module_load_include('inc', 'node', 'node.pages');
-$node = (object) array(
-  'uid' => $user->uid,
-  'name' => (isset($user->name) ? $user->name : ''),
-  'type' => 'supplier',
-  'language' => LANGUAGE_NONE,
-);
-$form = drupal_get_form('supplier_node_form', $node);
-
-print drupal_render($form)
-
-?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-tagsinput/1.3.6/jquery.tagsinput.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-tagsinput/1.3.6/jquery.tagsinput.min.js"></script>
 
