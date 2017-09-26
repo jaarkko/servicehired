b17aa8c36c423a96c0fb280dc09118038a0b55f9
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-26 21:15:40 +0300

    Sell

--- sites/all/modules/custom/servicehired_api/templates/sell.tpl.php
+++ sites/all/modules/custom/servicehired_api/templates/sell.tpl.php
@@ -4,10 +4,7 @@
 
 <div class="order-flow">
     <div class="left-col"><h3>How does Servicehired work?</h3>
-        <p>Write a sotry here.</p>
-
-        <h3>Payment history</h3>
-        <ul><?php print get_user_billing(); ?></ul>
+        <p>Write a story here.</p>
     </div>
     <div class="right-col">
   <h3>Start selling now</h3>
