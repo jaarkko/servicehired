d70fbafce343009c685be98f6820dd724f90ec05
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-09-27 23:16:23 +0300

    Template

--- sites/all/themes/servicehired/template.php
+++ sites/all/themes/servicehired/template.php
@@ -3,3 +3,12 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+
+function servicehired_preprocess_page(&$variables) {
+  if (isset($variables['node']->type)) {
+    // If the content type's machine name is "my_machine_name" the file
+    // name will be "page--my-machine-name.tpl.php".
+    $variables['theme_hook_suggestions'][] = 'page__' . $variables['node']->type;
+  }
+}
