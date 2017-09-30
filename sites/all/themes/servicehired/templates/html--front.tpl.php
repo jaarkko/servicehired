61241da7f945310317bbef52ea55b50458053902
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-10-01 01:46:59 +0300

    no forn

--- sites/all/themes/servicehired/templates/html--front.tpl.php
+++ sites/all/themes/servicehired/templates/html--front.tpl.php
@@ -1229,10 +1229,6 @@
           pips: {
             mode: 'range',
             density: 3,
-            format: wNumb({
-              decimals: 0,
-              prefix: '$ '
-            })
           },
           start: [min, max]
         });
