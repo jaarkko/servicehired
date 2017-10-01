53c46598157872c6e112d29b965d83a37cdd4c4f
Jarkko Oksanen <jarkkoksanen@gmail.com>
2017-10-01 17:48:47 +0300

    Adding a business page

--- sites/all/modules/custom/servicehired_api/templates/create_business.tpl.php
+++ sites/all/modules/custom/servicehired_api/templates/create_business.tpl.php
@@ -9,17 +9,15 @@
             <p>We need your business details so we can send customers your way, and can verify that you are a real business. This allows our thousands
                 of visitors to see and find you.</p>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-12">
             <h4>1. Use high quality images</h4>
             <p>The better the images, the more the sales. We have seen that people are drawn to nice colorful pictures. Remember this when adding your profile.</p>
         </div>
-        <div class="col-md-4">
-
+        <div class="col-md-12">
             <h4>2. Create a long description</h4>
             <p>A long description (not just two sentences) will be able to help people to find your content, as our search is based on it. Also, it helps the customer to make an educated decision.</p>
         </div>
-        <div class="col-md-4">
-
+        <div class="col-md-12">
             <h4>3. Add many specific tags</h4>
             <p>The better the tags, the more the sales. If you want to be found with odd keywords, remember to add all these in, as our search is based on them.</p>
         </div>
@@ -47,6 +45,15 @@
         width: 65%;
         float: left;
     }
+    button#edit-preview {
+        display: none;
+    }
+    button#edit-submit {
+        width: 95%;
+        font-size: 16px;
+        margin-right: 5%;
+        text-align: center;
+    }
     .left-col.row {
         width: 34%;
         margin-right: 2%;
@@ -57,6 +64,12 @@
         border: 1px solid #d2d2d2;
         margin-top: 20px;
         font-size: 16px;
+    }
+    .order-flow .col-md-12 {
+        margin-bottom: 30px;
+    }
+    .tabbable {
+        display: none;
     }
 
     .left-col .tabbable {
