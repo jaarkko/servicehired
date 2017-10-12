22a5699b0b232a1c1244ddbacdac57be08ab96f3
Marko Korhonen <marko.korhonen@druid.fi>
2017-10-12 15:18:55 +0300

    Add Pantheon DB settings

--- slim3/settings.php
+++ slim3/settings.php
@@ -6,6 +6,17 @@ include 'sites/default/settings.php';
 // Drupal default DB
 $db_config = $databases['default']['default'];
 
+// Pantheon
+if (defined('PANTHEON_ENVIRONMENT')) {
+    $db_config = [
+        'host' => getenv('DB_HOST'),
+        'database' => getenv('DB_HOST'),
+        'username' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+        'port' => getenv('DB_PORT'),
+    ];
+}
+
 return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
@@ -21,6 +32,7 @@ return [
             'dbname' => $db_config['database'],
             'user' => $db_config['username'],
             'pass' => $db_config['password'],
+            'port' => $db_config['port'],
         ],
 
     ],
